cba543a885586a83b54ea2ecb7ba845b00ca4de6
Yechan Bae <qwazpia@gmail.com>
2018-12-27 23:48:24 +0900

    Setup initial dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use clap::{App, Arg, crate_version, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("soma")
+        .version(crate_version!())
+        .about("Your one-stop CTF problem management tool")
+        .subcommand(SubCommand::with_name("hello")
+            .about("shows hello message"))
+        .get_matches();
+
+    match matches.subcommand() {
+        ("hello", _) => {
+            println!("soma hello!");
+        }
+        _ => {
+            println!("no hello...");
+        }
+    }
 }
