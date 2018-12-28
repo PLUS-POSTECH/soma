cfa0d4416c07a01943524a63cd1574665530752b
Yechan Bae <qwazpia@gmail.com>
2018-12-28 21:35:21 +0900

    Change shiplift to bollard

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,56 @@
-use std::str::FromStr;
+use bollard::Docker;
+use bollard::image::APIImages;
+use bollard::image::ListImagesOptions;
+use failure::Error;
+use hyper::client::connect::Connect;
+use tokio::runtime::current_thread::Runtime;
 
-use futures::future::Future;
-use http::uri::Uri;
-use lazy_static::lazy_static;
-use shiplift::builder::ContainerListOptions;
-use shiplift::Containers;
-use shiplift::Docker;
-use shiplift::rep::Container;
+type DockerResult<T> = Result<T, Error>;
 
-lazy_static! {
-    static ref DOCKER: Docker = if cfg!(windows) {
-            Docker::host(Uri::from_str("tcp://127.0.0.1:2375").unwrap())
-        } else {
-            Docker::new()
-        };
+pub trait Printer: Send + Sync {
+    type Handle;
+
+    fn get_current_handle(&self) -> Self::Handle;
+    fn write_line_at(&mut self, handle: &Self::Handle, message: &str);
+    fn write_line(&mut self, message: &str);
 }
 
-pub fn list() -> impl Future<Item = Vec<Container>, Error = shiplift::Error> {
-    let container = Containers::new(&DOCKER);
-    container.list(&ContainerListOptions::builder().all().build())
+pub struct Environment<C> {
+    docker: Docker<C>,
+    runtime: Runtime,
 }
+
+impl<C> Environment<C>
+    where C: 'static + Connect + Sync
+{
+    pub fn new(docker: Docker<C>, runtime: Runtime) -> Environment<C> {
+        Environment {
+            docker,
+            runtime,
+        }
+    }
+
+    pub fn list(&mut self) -> DockerResult<Vec<APIImages>> {
+        self.runtime.block_on(
+            self.docker.list_images(Some(ListImagesOptions::<String> {
+                all: true,
+                ..Default::default()
+            })))
+    }
+
+    pub fn pull(&mut self, printer: &mut impl Printer, image_name: &str) {}
+}
+
+/*
+pub fn create_hello() -> impl Future<Item=Vec<(serde_json::value::Value, ContainerCreateInfo)>, Error=shiplift::Error> {
+    let image_name = "hello-world";
+    let container_options = ContainerOptions::builder(image_name).build();
+
+    DOCKER.images().pull(&PullOptions::builder().image(image_name).tag("latest").build())
+        .and_then(move |pull_result| {
+            DOCKER.containers().create(&container_options).map(
+                |create_result| (pull_result, create_result)
+            )
+        }).collect()
+}
+*/
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,70 @@
+use bollard::Docker;
 use clap::{App, Arg, crate_version, SubCommand};
-use tokio::runtime::Runtime;
+use crossterm::{cursor, terminal, Terminal, TerminalCursor};
+use crossterm::terminal::ClearType;
+use hyper::client::connect::Connect;
+use tokio::runtime::current_thread::Runtime;
+use failure::Error;
+
+use soma::Environment;
+use soma::Printer;
+
+struct TerminalPrinter {
+    cursor: TerminalCursor<'static>,
+    terminal: Terminal<'static>,
+}
+
+impl TerminalPrinter {
+    fn new() -> TerminalPrinter {
+        TerminalPrinter {
+            cursor: cursor(),
+            terminal: terminal(),
+        }
+    }
+}
+
+impl Printer for TerminalPrinter {
+    type Handle = (u16, u16);
+
+    fn get_current_handle(&self) -> Self::Handle {
+        let handle = self.cursor.pos();
+        println!();
+        handle
+    }
+
+    fn write_line_at(&mut self, handle: &Self::Handle, message: &str) {
+        self.cursor.save_position();
+        self.cursor.goto(handle.0, handle.1);
+        self.terminal.clear(ClearType::CurrentLine);
+        println!("{}", message);
+        self.cursor.reset_position();
+    }
+
+    fn write_line(&mut self, message: &str) {
+        println!("{}", message);
+    }
+}
+
+#[cfg(windows)]
+fn connect_default() -> Result<Docker<impl Connect>, Error> {
+    Docker::connect_with_named_pipe_defaults()
+}
+
+#[cfg(unix)]
+fn connect_default() -> Result<Docker<impl Connect>, Error> {
+    Docker::connect_with_unix_defaults()
+}
+
+fn default_setup() -> (Environment<impl Connect>, TerminalPrinter) {
+    (
+        Environment::new(
+            connect_default().expect("failed to connect to docker"),
+            Runtime::new().expect("failed to create runtime"),
+        ),
+        TerminalPrinter::new(),
+    )
+}
+
 
 fn main() {
     let matches = App::new("soma")
@@ -7,16 +72,33 @@ fn main() {
         .about("Your one-stop CTF problem management tool")
         .subcommand(SubCommand::with_name("list")
             .about("shows the list of containers"))
+        .subcommand(SubCommand::with_name("create-hello")
+            .about("creates docker hello-world container"))
         .get_matches();
 
     match matches.subcommand() {
         ("list", _) => {
-            let mut runtime = Runtime::new().expect("failed to initialize the runtime");
-            let result = runtime.block_on(soma::list());
-            println!("{:#?}", result);
+            let (mut env, mut printer) = default_setup();
+            match env.list() {
+                Ok(images) => {
+                    let message = images.iter().map(|container| {
+                        format!("{:?}", container)
+                    }).collect::<Vec<_>>().join("\n");
+                    printer.write_line(&message);
+                }
+                Err(e) => {
+                    eprintln!("{:?}", e);
+                }
+            }
+        }
+        ("create-hello", _) => {
+            let (mut env, mut printer) = default_setup();
+            env.pull(&mut printer, "hello-world");
         }
         _ => {
-            println!("no hello...");
+            println!("I don't understand...");
         }
     }
+
+    let a = 1;
 }
