3f132184a617ce05a4e8e8129a8eda0ed1d00960
Kangsu Kim <pica744@gmail.com>
2019-01-02 19:10:00 +0900

    Remove repository struct dependency on image build function

--- templates/binary/start.sh
+++ templates/binary/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 export TERM=xterm
 # TODO: Container internal port settings may be implemented afterwards
-socat TCP-LISTEN:1024,pktinfo,reuseaddr,fork EXEC:"stdbuf -i 0 -o 0 {{ repository.manifest.binary.entry }}",stderr
+socat TCP-LISTEN:1024,pktinfo,reuseaddr,fork EXEC:"stdbuf -i 0 -o 0 {{ manifest.binary.entry }}",stderr
 sleep infinity;
