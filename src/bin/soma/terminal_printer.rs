06e604d76660d9fe50378652e9f6399efb6bb172
Kangsu Kim <pica744@gmail.com>
2019-01-21 21:07:59 +0900

    Use 'if let' instead of 'match'

--- src/bin/soma/terminal_printer.rs
+++ src/bin/soma/terminal_printer.rs
@@ -28,31 +28,29 @@ impl Printer for TerminalPrinter {
     }
 
     fn write_line_at(&mut self, handle: &Self::Handle, message: &str) {
-        match || -> SomaResult<()> {
+        let result = || -> SomaResult<()> {
             self.cursor.save_position()?;
             self.cursor.goto(handle.0, handle.1)?;
             self.terminal.clear(ClearType::CurrentLine)?;
             self.write_line(message);
             self.cursor.reset_position()?;
             Ok(())
-        }() {
-            Ok(_) => (),
-            Err(e) => {
-                eprintln!("Error on TerminalPrinter: {}", e);
-            }
+        }();
+
+        if let Err(e) = result {
+            eprintln!("Error on TerminalPrinter: {}", e);
         }
     }
 
     fn write_line(&mut self, message: &str) {
-        match || -> SomaResult<()> {
+        let result = || -> SomaResult<()> {
             self.terminal.write(message)?;
             self.terminal.write("\n")?;
             Ok(())
-        }() {
-            Ok(_) => (),
-            Err(e) => {
-                eprintln!("Error on TerminalPrinter: {}", e);
-            }
+        }();
+
+        if let Err(e) = result {
+            eprintln!("Error on TerminalPrinter: {}", e);
         }
     }
 }
