b72f0b08f5162a7a91f94cc88c3ae7541b2fd394
Yechan Bae <qwazpia@gmail.com>
2019-02-13 17:28:34 +0900

    Add soma list tests

--- tests/add_remove.rs
+++ tests/add_remove.rs
@@ -13,13 +13,8 @@ fn test_add_remove() {
     let mut env = test_env(&mut data_dir);
     let mut runtime = default_runtime();
 
-    let repo_name = "simple-bof";
-    assert!(add(
-        &mut env,
-        "https://github.com/PLUS-POSTECH/simple-bof.git",
-        None
-    )
-    .is_ok());
+    let repo_name = SIMPLE_BOF_REPO_NAME;
+    assert!(add(&mut env, SIMPLE_BOF_GIT, None).is_ok());
 
     assert!(env.repo_manager().repo_exists(repo_name));
     let local_path = env
@@ -39,12 +34,7 @@ fn test_add_with_name() {
     let mut env = test_env(&mut data_dir);
 
     let repo_name = "complicated-bof";
-    assert!(add(
-        &mut env,
-        "https://github.com/PLUS-POSTECH/simple-bof.git",
-        Some(repo_name),
-    )
-    .is_ok());
+    assert!(add(&mut env, SIMPLE_BOF_GIT, Some(repo_name)).is_ok());
 
     assert!(env.repo_manager().repo_exists(repo_name));
     let local_path = env
@@ -64,19 +54,9 @@ fn test_prob_search() {
     let repo_name_1 = "bof1";
     let repo_name_2 = "bof2";
 
-    assert!(add(
-        &mut env,
-        "https://github.com/PLUS-POSTECH/simple-bof.git",
-        Some(repo_name_1)
-    )
-    .is_ok());
+    assert!(add(&mut env, SIMPLE_BOF_GIT, Some(repo_name_1)).is_ok());
 
-    assert!(add(
-        &mut env,
-        "https://github.com/PLUS-POSTECH/simple-bof.git",
-        Some(repo_name_2)
-    )
-    .is_ok());
+    assert!(add(&mut env, SIMPLE_BOF_GIT, Some(repo_name_2)).is_ok());
 
     assert_matches!(
         env.repo_manager()
@@ -95,4 +75,38 @@ fn test_prob_search() {
             .map_err(error_downcast),
         Err(Ok(SomaError::ProblemNotFound))
     );
+}
+
+#[test]
+fn test_soma_list() {
+    let (_, mut data_dir) = temp_data_dir();
+    let mut env = test_env(&mut data_dir);
+    let mut runtime = default_runtime();
+
+    let repo_name = BATA_LIST_REPO_NAME;
+    assert!(add(&mut env, BATA_LIST_GIT, None).is_ok());
+
+    assert!(env.repo_manager().repo_exists(repo_name));
+    let local_path = env
+        .repo_manager()
+        .get_repo(repo_name)
+        .expect("Added repository does not exist")
+        .path();
+    assert!(dir_contents_exists(local_path, &[".git"]));
+
+    assert!(env.repo_manager().search_prob("xkcd").is_ok());
+    assert!(env.repo_manager().search_prob("r0pbaby").is_ok());
+    assert!(env
+        .repo_manager()
+        .search_prob(&format!("{}.babyecho", BATA_LIST_REPO_NAME))
+        .is_ok());
+    assert_matches!(
+        env.repo_manager()
+            .search_prob("simple-bof")
+            .map_err(error_downcast),
+        Err(Ok(SomaError::ProblemNotFound))
+    );
+
+    assert!(remove(&mut env, repo_name, &mut runtime).is_ok());
+    assert!(!env.repo_manager().repo_exists(repo_name));
 }
--- tests/fetch.rs
+++ tests/fetch.rs
@@ -5,20 +5,45 @@ pub use self::common::*;
 mod common;
 
 #[test]
-fn test_fetch() {
+fn test_fetch1() {
     let temp_copy_dir = tempdir();
 
     let (_, mut data_dir) = temp_data_dir();
     let mut env = test_env(&mut data_dir);
 
-    let repo_name = "simple-bof";
-    assert!(add(
-        &mut env,
-        "https://github.com/PLUS-POSTECH/simple-bof.git",
-        None
-    )
-    .is_ok());
-    assert!(fetch(&env, repo_name, &temp_copy_dir).is_ok());
+    assert!(add(&mut env, SIMPLE_BOF_GIT, None).is_ok());
+    assert!(fetch(&env, "simple-bof", &temp_copy_dir).is_ok());
 
     expect_dir_contents(&temp_copy_dir, &["simple-bof"]);
+}
+
+#[test]
+fn test_fetch2() {
+    let temp_copy_dir = tempdir();
+
+    let (_, mut data_dir) = temp_data_dir();
+    let mut env = test_env(&mut data_dir);
+
+    assert!(add(&mut env, BATA_LIST_GIT, None).is_ok());
+    assert!(fetch(&env, "xkcd", &temp_copy_dir).is_ok());
+
+    expect_dir_contents(&temp_copy_dir, &["xkcd"]);
+}
+
+#[test]
+fn test_fetch3() {
+    let temp_copy_dir = tempdir();
+
+    let (_, mut data_dir) = temp_data_dir();
+    let mut env = test_env(&mut data_dir);
+
+    assert!(add(&mut env, BATA_LIST_GIT, None).is_ok());
+    assert!(fetch(
+        &env,
+        &format!("{}/r0pbaby", BATA_LIST_REPO_NAME),
+        &temp_copy_dir
+    )
+    .is_ok());
+
+    expect_dir_contents(&temp_copy_dir, &["r0pbaby"]);
 }
