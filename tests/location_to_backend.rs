ae06ec717d6ecc65f7b0060ab6aa439f61a572c3
Yechan Bae <qwazpia@gmail.com>
2019-02-17 00:17:54 +0900

    Restore backend variant check

--- tests/location_to_backend.rs
+++ tests/location_to_backend.rs
@@ -4,9 +4,16 @@ pub use self::common::*;
 
 mod common;
 
-fn test_parse(location: &str, expected_repo_name: &str) {
-    let (repo_name, _) = location_to_backend(location).expect("failed to parse the location");
+fn test_parse_git(location: &str, expected_repo_name: &str) {
+    let (repo_name, backend) = location_to_backend(location).expect("failed to parse the location");
     assert_eq!(repo_name, expected_repo_name);
+    assert!(backend.to_string().starts_with("Git"));
+}
+
+fn test_parse_local(location: &str, expected_repo_name: &str) {
+    let (repo_name, backend) = location_to_backend(location).expect("failed to parse the location");
+    assert_eq!(repo_name, expected_repo_name);
+    assert!(backend.to_string().starts_with("Local"));
 }
 
 fn test_parse_fail(location: &str) {
@@ -15,13 +22,13 @@ fn test_parse_fail(location: &str) {
 
 #[test]
 fn location_to_backend_success() {
-    test_parse(SIMPLE_BOF_GIT, SIMPLE_BOF_REPO_NAME);
-    test_parse(BATA_LIST_GIT, BATA_LIST_REPO_NAME);
+    test_parse_git(SIMPLE_BOF_GIT, SIMPLE_BOF_REPO_NAME);
+    test_parse_git(BATA_LIST_GIT, BATA_LIST_REPO_NAME);
     // TODO: git through other protocols
     // test_parse_git("git@github.com:PLUS-POSTECH/simple-bof.git", SIMPLE_BOF_REPO_NAME);
 
-    test_parse("ci", "ci");
-    test_parse("tests/common", "common");
+    test_parse_local("ci", "ci");
+    test_parse_local("tests/common", "common");
 }
 
 #[test]
